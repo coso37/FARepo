06eedf3f3743de148a0f4341ff03465cbbf8b452
fueledbyflux <fueledbyflux@gmail.com>
2015-01-29 16:06:53 +1200

    riven 5.2

--- FuckingAwesomeRiven/FuckingAwesomeRiven/SpellHandler.cs
+++ FuckingAwesomeRiven/FuckingAwesomeRiven/SpellHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using LeagueSharp.Common.Data;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
